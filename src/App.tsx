7677e97fe077e806b02a8e4ddc2b172953ec72e5
Furuta Naoki <fn.nk.2002@gmail.com>
2024-08-21 23:31:12 +0900

    テキストサイズに合わせて伸びる量が変わる

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
         ></StretchingIcon>
         <StretchingIcon
           src="/favicon.ico"
-          text="Sample3"
+          text="Sample312312341234"
         ></StretchingIcon>
       </div>
     </div>
--- src/components/StretchingIcon/index.tsx
+++ src/components/StretchingIcon/index.tsx
@@ -1,6 +1,7 @@
 import Icon from '../Icon'
 import clsx from 'clsx'
 import styles from './StretchingIcon.module.css'
+import { useState, useEffect, useRef } from 'react'
 
 type StretchingIconProps = {
   src: string
@@ -8,15 +9,46 @@ type StretchingIconProps = {
 }
 
 function StretchingIcon(props: StretchingIconProps) {
+  const [width, setWidth] = useState(0)
+  const [isHovered, setIsHovered] = useState(false)
+  const elementRef = useRef<HTMLDivElement>(null)
+  const textRef = useRef<HTMLSpanElement>(null)
+
+  const handleMouseEnter = () => {
+    setIsHovered(true)
+    if (elementRef.current) {
+      elementRef.current.style.width = `${width + 60}px`
+    }
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false)
+    if (elementRef.current) {
+      elementRef.current.style.width = `${50}px`
+    }
+  }
+
+  useEffect(() => {
+    if (textRef.current) {
+      setWidth(textRef.current.offsetWidth)
+    }
+  }, [])
+
   return (
     <div className={clsx(styles['icon-container'])}>
-      <div className={clsx(styles['icon-wrapper'])}>
+      <div
+        ref={elementRef}
+        className={clsx(styles['icon-wrapper'])}
+        onClick={() => {console.log(width)}}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
         <Icon
           src={props.src}
           trim="round"
           width="50px"
         />
-        <span className={clsx(styles['icon-text'])}>{props.text}</span>
+        <span ref={textRef} className={clsx(styles['icon-text'])}>{props.text} : {width}</span>
       </div>
     </div>
   )
