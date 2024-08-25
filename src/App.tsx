3577f25ca0b07b413c14ca75d6cb754918b97982
Furuta Naoki <fn.nk.2002@gmail.com>
2024-08-25 23:42:59 +0900

    react-markdown導入

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,25 @@ import ProfileLinkList from './components/ProfileLinkList'
 function App() {
   return (
     <div className="">
-      <Icon
-        src="/favicon.ico"
-        trim="round"
-        borderBold="4px"
-        borderColor="rgba(78, 78, 78, 1)"
-        width="150px"
-      />
-      <h1>zzz' home</h1>
-      <div>
-        <h3>↓ Click the icon to explore more information. ↓</h3>
-        <ProfileLinkList />
-      </div>
+      <main>
+        <div className="title-area">
+          <Icon
+            src="/favicon.ico"
+            trim="round"
+            borderBold="4px"
+            borderColor="rgba(78, 78, 78, 1)"
+            width="150px"
+          />
+          <h1>zzz' home</h1>
+        </div>
+        <div className="link-list">
+          <h3>↓ Click the icon to explore more information. ↓</h3>
+          <ProfileLinkList />
+        </div>
+      </main>
+      <footer>
+        <p>© 2024 zzz</p>
+      </footer>
     </div>
   )
 }
