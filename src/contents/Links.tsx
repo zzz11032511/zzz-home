f25d3af28ed650e92ec6230d14f732d9c738267a
Furuta Naoki <fn.nk.2002@gmail.com>
2024-08-28 20:52:23 +0900

    Links

--- src/contents/Links.tsx
+++ src/contents/Links.tsx
@@ -3,7 +3,11 @@ import ReactMarkdown from 'react-markdown'
 const markdownString = `
 # Links
 
-色々書く予定
+- [ブログ](https://zzz-blog.netlify.app)
+- [GitHub](https://github.com/zzz11032511)
+- [Rate Your Music](https://rateyourmusic.com/~zzz11032511)
+- [Glitchwave](https://glitchwave.com/user/zzz11032511)
+- [Spotify](https://open.spotify.com/user/vvwgu4170o9y06cusxs427fcy?si=e3fadab248c74a82)
 `
 
 function Links() {
