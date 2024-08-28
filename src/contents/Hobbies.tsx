beee99e8e5035cb078afbab70cf06c946ea33653
Furuta Naoki <fn.nk.2002@gmail.com>
2024-08-28 17:53:32 +0900

    Hobbies

--- src/contents/Hobbies.tsx
+++ src/contents/Hobbies.tsx
@@ -3,7 +3,30 @@ import ReactMarkdown from 'react-markdown'
 const markdownString = `
 # Hobbies
 
-色々書く予定
+zzzの趣味を大公開！
+
+### 好きなミュージシャン・作曲家・バンド
+- The Beach Boys
+- The Beatles
+- Boards of Canada
+- Broadcast
+- Carole King
+- The Kinks
+- Silver Apples
+- Portishead
+- 植松伸夫
+- 竹村延和
+- たま
+- 平沢進
+- 細野晴臣
+
+### 好きなアニメ
+- カペリート
+- スポンジボブ
+- ジャムハウスネイル
+- 楽しいムーミン一家
+- PUIPUIモルカー(1期)
+
 `
 
 function Hobbies() {
